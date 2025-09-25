a89283c3c79c137c832a6eda583b23fe000f0090
Assciil <gross8@hm.edu>
2025-09-25 19:56:54 +0200

    test4

---
+++ SX126X_LoRa_HAT_Code/raspberrypi/python/Datenstation.py
@@ -0,0 +1,14 @@
+import sys
+import sx126x
+import threading
+import time
+import select
+import termios
+import tty
+from threading import Timer
+
+old_settings = termios.tcgetattr(sys.stdin)
+tty.setcbreak(sys.stdin.fileno())
+
+if __name__ == "__main__":
+    print("Hello World")
