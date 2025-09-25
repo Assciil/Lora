05f510829d5f33cb7d2cdc615c420622e1eb5dba
Assciil <gross8@hm.edu>
2025-09-25 20:37:32 +0200

    if condition

--- SX126X_LoRa_HAT_Code/raspberrypi/python/Datenstation.py
+++ SX126X_LoRa_HAT_Code/raspberrypi/python/Datenstation.py
@@ -11,4 +11,5 @@ if __name__ == "__main__":
     node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=868,addr=0,power=22,rssi=True,air_speed=2400,relay=False)
     while True:
         text = node.receive()
-        print("Received:", text)
+        if text is not None:
+            print("Received:", text)
