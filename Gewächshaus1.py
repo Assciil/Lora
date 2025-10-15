4f26baf31cbb402f2aae143dc287a176f92795e8
Assciil <gross8@hm.edu>
2025-10-15 21:27:15 +0200

    .

--- Gewächshaus1.py
+++ Gewächshaus1.py
@@ -23,8 +23,7 @@ import time
 
 
 if __name__ == "__main__":
-    receive_adress = 65535  
-    node = sx126x(serial_num = "/dev/ttyS0",freq=868,addr=65535,power=22,rssi=True,air_speed=2400, buffer_size=128, relay=False, duty_cycle=0.01)
+    node = sx126x(serial_num = "/dev/ttyS0",freq=868,addr=63535,power=22,rssi=True,air_speed=2400, buffer_size=128, relay=False, duty_cycle=0.01)
     while True:
         node.send_float(64535, 868, 23.5)  
         time.sleep(10)  # Warte 20 s bis zum nächsten Senden
