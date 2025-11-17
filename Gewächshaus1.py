34d42f8ad914d558f75689f018b96970db5be4fc
Assciil <gross8@hm.edu>
2025-11-17 15:36:45 +0100

    send

--- Gewächshaus1.py
+++ Gewächshaus1.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     string = "Hallo Willi, wie geht's? Lange schon nichts mehr von dir gehört. Ich hoffe, alles ist in Ordnung bei dir. Melde dich mal wieder. Liebe Grüße! From Lisa."
     node = sx126x(serial_num = "/dev/ttyS0",freq=868,addr=63535,power=22,rssi=True,air_speed=2400, relay=False, duty_cycle=0.01, buffer_size = 32)
     while True:
-        node.send_string(64535, 868, string)  
-        #time.sleep(2)  
+        node.send_string(64535, 868, string)
+        print("Sent")
+        time.sleep(1)  
         
