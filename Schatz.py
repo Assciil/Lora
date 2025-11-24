923f38ba15179fe21eb8d68ebb50efabaccb7ba6
Assciil <gross8@hm.edu>
2025-11-24 15:36:46 +0100

    sweeper und schatz

--- Schatz.py
+++ Schatz.py
@@ -4,11 +4,11 @@ import time
 if __name__ == "__main__":
     counter = 0
     schatz = {
-        1: ("Schatz 1: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 862),
-        2: ("Schatz 2: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 862),
-        3: ("Schatz 3: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 862),
-        4: ("Schatz 4: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 862),
-        5: ("Schatz 5: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 862)
+        1: ("Schatz 1: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64534, 862),
+        2: ("Schatz 2: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 863),
+        3: ("Schatz 3: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64534, 864),
+        4: ("Schatz 4: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 865),
+        5: ("Schatz 5: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 868)
     }
 
     node = sx126x(serial_num = "/dev/ttyS0",freq=868,addr=63535,power=22,rssi=True,air_speed=2400, relay=False, duty_cycle=0.01, buffer_size = 32)
@@ -20,5 +20,4 @@ if __name__ == "__main__":
             time.sleep(1.1)
 
         print("Counter: ", counter)
-        time.sleep(5)
         counter += 1  
