02e04e32b0a1eef2ff4fd671c2aea629475275dc
Assciil <gross8@hm.edu>
2025-11-24 15:53:36 +0100

    test

--- Schatz.py
+++ Schatz.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         #2: ("Schatz 2: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 863),
         #3: ("Schatz 3: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64534, 864),
         #4: ("Schatz 4: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 865),
-        5: ("Schatz 5: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64535, 868)
+        5: ("Schatz 5: liegt in Raum XYZ bei dem Pult. Du kannst es abholen und 62234 nach dem Code fragen.", 64534, 863)
     }
 
     node = sx126x(serial_num = "/dev/ttyS0",freq=868,addr=63535,power=22,rssi=True,air_speed=2400, relay=False, duty_cycle=0.01, buffer_size = 32)
